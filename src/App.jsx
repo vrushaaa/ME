8a86b15316c75cfbdeb6e52815f64326a835a374
Vrusha Naik <15vrusha@gmail.com>
2025-07-14 22:03:52 +0530

    App.jsx with welcome msg

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css' ;
-import { LoadingScreen } from './components/LoadingScreen';
+import './index.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <LoadingScreen/>
-    </>
+function App(){
+  return(
+    <div className='min-h-screen bg-gray-50 flex items-center justify-center'>
+      <h1 className='text-3xl font-bold text-blue-600'>
+        Welcome to My Portfolio Website!
+      </h1>
+    </div>
   )
 }
 
